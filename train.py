55b240b7ae0c844cd1d51b2443220b33ffa05057
SaranchaiZa <115490161+JEngHAng@users.noreply.github.com>
2025-11-09 23:27:22 +0700

    epochs=200, imgsz=896, batch=8, model = YOLO("last.pt") เทรนเพิ่ม 0.889 0.646

--- train.py
+++ train.py
@@ -1,28 +1,29 @@
 from ultralytics import YOLO
 
 def main():
-    model = YOLO("yolov8n.pt")
+    model = YOLO("last.pt")
 
     # เริ่มเทรนโมเดล
     model.train(
         data="food-detect-8/data.yaml",  # path ไปยัง dataset
         epochs=200,                      # จำนวนรอบการเทรน 
-        imgsz=640,                       # ขนาดภาพระหว่างเทรน
-        batch=16,                        # จำนวนภาพต่อ batch
+        imgsz=896,                       # ขนาดภาพระหว่างเทรน
+        batch=8,                        # จำนวนภาพต่อ batch
         device="cuda",                   # ใช้ GPU ถ้ามี (cuda), ถ้าไม่มีจะใช้ cpu อัตโนมัติ
         name="food_detect_model",        # ชื่อโฟลเดอร์สำหรับบันทึกผลเทรน
         workers=4,                       # จำนวน thread สำหรับโหลดข้อมูล (มากขึ้นถ้า CPU แรง)
         amp=True,                        # ใช้ Automatic Mixed Precision เทรนเร็วและกินแรมน้อยลง
         augment=True,                    # เปิด data augmentation เพื่อเพิ่มความหลากหลายของข้อมูล
-        lr0=0.002,                       # ค่าเริ่มต้น learning rate (ปรับขึ้นเล็กน้อยให้เรียนรู้เร็ว)
+        lr0=0.0003,                      # ค่าเริ่มต้น learning rate (ปรับขึ้นเล็กน้อยให้เรียนรู้เร็ว)
         lrf=0.01,                        # ค่า learning rate ปลายทางเมื่อเทรนใกล้จบ
         momentum=0.937,                  # ใช้ momentum ค่ามาตรฐาน YOLO (ช่วยให้การเรียนรู้เสถียร)
         weight_decay=0.0005,             # ป้องกัน overfitting โดยลดน้ำหนัก parameter ที่ไม่สำคัญ
-        patience=50,                     # ถ้า val loss ไม่ดีขึ้นเกิน 50 รอบ จะหยุดเทรนอัตโนมัติ
+        patience=30,                     # ถ้า val loss ไม่ดีขึ้นเกิน 30 รอบ จะหยุดเทรนอัตโนมัติ
         val=True,                        # ให้ validate หลังแต่ละ epoch เพื่อดู performance
         seed=42,                         # ตั้งค่า random seed ให้เทรนได้ผลซ้ำได้
         cos_lr=True,                     # ใช้ cosine learning rate schedule เพื่อการลดค่า lr อย่างนุ่มนวล
-        cache=True                       # โหลด dataset ไว้ใน RAM เพื่อเพิ่มความเร็วในการเทรน
+        cache=True,                      # โหลด dataset ไว้ใน RAM เพื่อเพิ่มความเร็วในการเทรน
+        resume=False
     )
 
     # เมื่อเทรนเสร็จจะได้ไฟล์ weights เช่น:
